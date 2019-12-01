4e6290454b24cc11f50d4928dfa7805964284fb4
Bartosz Szar <48298481+szarbartosz@users.noreply.github.com>
2019-12-01 20:36:15 +0100

    powtorka lab1

--- kolos_sql/podstawy_cw_koncowe.sql
+++ kolos_sql/podstawy_cw_koncowe.sql
@@ -52,7 +52,8 @@ order by title
 
 --2. Napisz polecenie, które:
 --		-wybiera numer członka biblioteki, isbn książki i wartość
---		 naliczonej kary dla wszystkich wypożyczeń, dla których naliczono karę--		-stwórz kolumnę wyliczeniową zawierającą podwojoną wartość kolumny fine_assessed
+--		 naliczonej kary dla wszystkich wypożyczeń, dla których naliczono karę
+--    -stwórz kolumnę wyliczeniową zawierającą podwojoną wartość kolumny fine_assessed
 --		-stwórz alias ‘double fine’ dla tej kolumny
 select member_no, isbn, fine_assessed, 2 * fine_assessed as 'double fine'
 from loanhist
